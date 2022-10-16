f7ba9023efe1fca8e43430f1fb43f99c0f9c43c9
iman370 <imaniqbal@live.co.uk>
2022-10-17 00:56:55 +0100

    Loads images

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import numpy as np
+from PIL import Image
+
+ascii_alphabet = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+
+def loadImage(image):
+    return np.array(Image.open(image).convert('L').resize((200,200)))
 
 def main():
+    print(loadImage('amongus.png'))
     return 0
 
 
