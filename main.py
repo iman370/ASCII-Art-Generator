e349273da49f4c1dd111e905624e4a97f4e9d347
iman370 <imaniqbal@live.co.uk>
2022-10-17 01:35:22 +0100

    Changed numbers to variables

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from PIL import Image
 
 # Turn this image into ASCII art
 img = 'amongus.png'
+imageX = 32
+imageY = 32
 
 ascii_alphabet = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. " #length: 70
 
-def loadImage(image, resizeX=64, resizeY=64):
+def loadImage(image, resizeX=imageX, resizeY=imageY):
     return np.array(Image.open(image).convert('L').resize((resizeX,resizeY)))
 
 def showImage(image):
@@ -19,14 +21,11 @@ def main():
     image = (image/255) * 70
     #showImage(image)
     ascii_image = ""
-    for i in range(64):
-        for j in range(64):
+    for i in range(imageX):
+        for j in range(imageY):
             ascii_image += ascii_alphabet[round(image[i][j]) - 1]
         ascii_image += "\n"
     print(ascii_image)
-
-
-
 
 if __name__ == '__main__':
     main()
