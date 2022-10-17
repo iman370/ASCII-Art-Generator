9da80c90edfb1b99e06e33b0f462f9819c724776
iman370 <imaniqbal@live.co.uk>
2022-10-17 01:31:09 +0100

    Creates ASCII art

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import numpy as np
 from PIL import Image
 
-ascii_alphabet = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+# Turn this image into ASCII art
+img = 'amongus.png'
 
-def loadImage(image):
-    return np.array(Image.open(image).convert('L').resize((200,200)))
+ascii_alphabet = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. " #length: 70
+
+def loadImage(image, resizeX=64, resizeY=64):
+    return np.array(Image.open(image).convert('L').resize((resizeX,resizeY)))
+
+def showImage(image):
+    from matplotlib import pyplot as plt
+    plt.imshow(image, interpolation='nearest')
+    plt.show()
 
 def main():
-    print(loadImage('amongus.png'))
-    return 0
+    image = loadImage(img)
+    image = (image/255) * 70
+    #showImage(image)
+    ascii_image = ""
+    for i in range(64):
+        for j in range(64):
+            ascii_image += ascii_alphabet[round(image[i][j]) - 1]
+        ascii_image += "\n"
+    print(ascii_image)
+
+
 
 
 if __name__ == '__main__':
